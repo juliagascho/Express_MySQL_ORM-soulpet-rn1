58c488567961ec517bd46598a1f71282da4fbec5
juliagascho <juliagascho@hotmail.com>
2023-04-19 09:54:49 -0300

    Finalização CRUD pets

--- index.js
+++ index.js
@@ -130,6 +130,39 @@ app.put("/clientes/:id", async (req, res) => {
     }
 })
 
+// rota para atualizar um pet:
+app.put("/pets/:id", async (req, res) => {
+    // dados que virão no corpo JSON para atualizar:
+    const { nome, tipo, dataNasc, porte } = req.body; 
+
+    //primeiro ver se o pet existe: findOne ou findByPk
+    // findByPk vai fazer um SELECT * FROM pets WHERE id = "req.params.id";    
+    const pet = await Pet.findByPk(req.params.id); // se desestruturasse como no atualizar clientes, poderia usar apenas id aqui
+
+    //dentro de pets ou vai ter o pet que está buscando, ou não vai ter nada
+    // se pet for nulo, não existe pet com o id
+    // se pet é null -> não existe o pet
+    try {
+        if(pet) {
+            //atualiza
+            // dentro do model Pet tem várias opções, para atualizar usa o update  
+            // IMPORTANTE: indicar qual pet a ser atualizado 
+            // UPDATE RECEBE 2 ARGUMENTOS: 1º DADOS NOVOS, 2º WHERE     
+            await Pet.update({ nome, tipo, dataNasc, porte }, { where: { id: req.params.id } }); // where id = req.params.id (coluna da tabela)
+            // outra maneira de fazer: await pet.update ({ nome, tipo, dataNasc, porte })
+            res.status(200).json({ message: "Pet atualizado." });
+        } else {
+            // retorna 404
+            // se id for inválido: a resposta ao cliente será essa
+            res.status(404).json({ message: "Pet não encontrado." });
+        }
+    } catch (err) {
+        // retorna 500
+        // erro inesperado: mensagem será essa
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }
+})
+
 
 // excluir um cliente
 app.delete("/clientes/:id", async (req, res) => {
@@ -146,6 +179,25 @@ app.delete("/clientes/:id", async (req, res) => {
         res.status(500).json({ message: "Um erro aconteceu." });
     }    
 });
+
+//rota para deletar pet
+// apenas verificar se o pet existe
+app.delete("/pets/:id", async (req, res) => {
+    //checar se o pet existe 
+    const pet = await Pet.findByPk(req.params.id);
+    try {
+        if(pet) {
+            //pet existe, podemos apagar
+            await pet.destroy(); //não precisa passar nada, pq ele já sabe quem é o pet, objeto pet
+            res.status(200).json({ message: "Pet removido." });
+        } else {
+            res.status(404).json({ message: "Pet não encontrado." });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }    
+});
+
 
 // Escuta de eventos (listen)
 app.listen(3000, () => {
